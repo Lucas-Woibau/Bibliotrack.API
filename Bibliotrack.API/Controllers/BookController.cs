4e0128617d26cb774ace15ea4da1a2e684169083
Lucas <lucaswoibau7@gmail.com>
2025-11-17 09:28:25 -0300

    faet: controllers finished.

--- Bibliotrack.API/Controllers/BookController.cs
+++ Bibliotrack.API/Controllers/BookController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bibliotrack.Application.Commands.BookCommands.AddBook;
+using Bibliotrack.Application.Commands.BookCommands.DeleteBook;
+using Bibliotrack.Application.Commands.BookCommands.UpdateBook;
+using Bibliotrack.Application.Queries.Book.GetBooksById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bibliotrack.API.Controllers
 {
@@ -6,6 +11,64 @@ namespace Bibliotrack.API.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        public IMediator _mediator;
 
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = new AddBookCommand();
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetBookByIdQuery(id));
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBookCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data }, command);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateBookCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _mediator.Send(new DeleteBookCommand(id));
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok();
+        }
     }
 }
--- Bibliotrack.API/Controllers/LoanController.cs
+++ Bibliotrack.API/Controllers/LoanController.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bibliotrack.Application.Commands.BookCommands.AddBook;
+using Bibliotrack.Application.Commands.BookCommands.DeleteBook;
+using Bibliotrack.Application.Commands.BookCommands.UpdateBook;
+using Bibliotrack.Application.Commands.LoanCommands.AddLoan;
+using Bibliotrack.Application.Commands.LoanCommands.DeleteLoan;
+using Bibliotrack.Application.Commands.LoanCommands.UpdateLoan;
+using Bibliotrack.Application.Queries.Book.GetBooksById;
+using Bibliotrack.Application.Queries.LoanQueries.GetAllLoans;
+using Bibliotrack.Application.Queries.LoanQueries.GetLoanById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bibliotrack.API.Controllers
 {
@@ -6,5 +16,64 @@ namespace Bibliotrack.API.Controllers
     [ApiController]
     public class LoanController : ControllerBase
     {
+        public IMediator _mediator;
+
+        public LoanController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = new GetAllLoansQuery();
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetLoanByIdQuery(id));
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddLoanCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data }, command);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateLoanCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _mediator.Send(new DeleteLoanCommand(id));
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok();
+        }
     }
 }
