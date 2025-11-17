30c46cf611e5f9e22307adf47ae5208e8966d102
Lucas <lucaswoibau7@gmail.com>
2025-11-17 09:28:25 -0300

    faet: controllers finished.

--- Bibliotrack.Infrastructure/Persistence/BibliotrackDbContext.cs
+++ Bibliotrack.Infrastructure/Persistence/BibliotrackDbContext.cs
@@ -10,5 +10,12 @@ namespace Bibliotrack.Infrastructure.Persistence
         public DbSet<Book>  Books { get; set; }
         public  DbSet<User> Users{ get; set; }
         public DbSet<Loan> Loans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+
+            base.OnModelCreating(builder);
+        }
     }
 }
