2497bc608502d4915f47a095d0979a4c164db676
Lucas <lucaswoibau7@gmail.com>
2025-11-21 11:15:57 -0300

    refactor: IEntityTypeConfiguration added to entities.

---
+++ Bibliotrack.Infrastructure/Persistence/Configurations/LoanConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Bibliotrack.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bibliotrack.Infrastructure.Persistence.Configurations
+{
+    public class LoanConfiguration : IEntityTypeConfiguration<Loan>
+    {
+        public void Configure(EntityTypeBuilder<Loan> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(e => e.Book)
+                    .WithMany()
+                    .HasForeignKey(e => e.IdBook);
+        }
+    }
+}
