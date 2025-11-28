117736806b1327dabed538077242b6f7d143f69a
Lucas <lucaswoibau7@gmail.com>
2025-11-27 22:57:51 -0300

    feat: added computerHash function.

---
+++ Bibliotrack.Application/Commands/UserCommands/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,24 @@
+﻿using Bibliotrack.Application.Models;
+using MediatR;
+
+namespace Bibliotrack.Application.Commands.UserCommands.LoginUser
+{
+    public class LoginUserCommand : IRequest<ResultViewModel>
+    {
+        public LoginUserCommand(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, ResultViewModel>
+    {
+        public Task<ResultViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
